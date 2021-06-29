45e083f960e49aecc96808940907dea8bcb7f74f
Priscila Conchou <priscilaguedesdantas@gmail.com>
2021-06-29 21:38:11 +0200

    Ajout de la methode controleInscription dans UtilisateurManager

--- src/fr/eni/projet/bll/UtilisateurManager.java
+++ src/fr/eni/projet/bll/UtilisateurManager.java
@@ -27,5 +27,52 @@ public class UtilisateurManager {
 	public static void insert(Utilisateur u) {
 		daoUtilisateur.insert(u);
 	}
+	
+	public static boolean controleInscription(Utilisateur u, String confirmMotDePasse) {
+		
+		List<Utilisateur> utilisateurs = daoUtilisateur.selectAll();
+		
+		// check mot de passe
+		if(!u.getMotDePasse().equals(confirmMotDePasse)) {
+			System.out.println("erreur confimation mot de passe");
+			return false;
+		}
+		
+		//check pseudo
+		for (Utilisateur ut : utilisateurs) {
+			if(ut.getPseudo().equals(u.getPseudo())) {
+				System.out.println("pseudo déjà utilisé");
+				return false;
+			}
+		}
+		
+		//check e-mail
+		for (Utilisateur ut : utilisateurs) {
+			if(ut.getEmail().equals(u.getEmail())) {
+				System.out.println("email déjà utilisé");
+				return false;
+			}
+		}
+		
+		//check telephone
+		if(u.getTelephone()!=null) {
+			for (Utilisateur ut : utilisateurs) {
+				if(ut.getTelephone().equals(u.getTelephone())) {
+					System.out.println("telephone déjà utilisé");
+					return false;
+				}
+			}	
+		}
+		
+		
+		
+		return true;
+	}
+	
+	
+	
+	
+	
+	
 }
 	 
