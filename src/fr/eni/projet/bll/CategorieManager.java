373696fbb4a9a13aa05e505302c05e7343481a7f
Priscila Conchou <priscilaguedesdantas@gmail.com>
2021-07-02 22:08:09 +0200

    Corrections de généricité
    Signed-off-by: Priscila Conchou <priscilaguedesdantas@gmail.com>

--- src/fr/eni/projet/bll/CategorieManager.java
+++ src/fr/eni/projet/bll/CategorieManager.java
@@ -2,14 +2,15 @@ package fr.eni.projet.bll;
 
 import java.sql.SQLException;
 
+
 import java.util.List;
 
 import fr.eni.projet.bo.Categorie;
-import fr.eni.projet.dal.DAO;
+import fr.eni.projet.dal.DAOCategorie;
 import fr.eni.projet.dal.DAOFactory;
  
 public class CategorieManager {
-	private static DAO<Categorie> daoCategorie = (DAO<Categorie>) DAOFactory.getCategorieDAO();
+	private static DAOCategorie daoCategorie = DAOFactory.getCategorieDAO();
 	
 	public static List<Categorie> selectAll() throws SQLException {
 		return daoCategorie.selectAll();
