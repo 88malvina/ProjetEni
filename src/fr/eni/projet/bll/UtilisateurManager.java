099da774c4d7eabba1abe600f7538d8e3c13f3bd
Priscila Conchou <priscilaguedesdantas@gmail.com>
2021-07-02 22:41:57 +0200

    Actualisation Servlet et UtilisateurManager
    Signed-off-by: Priscila Conchou <priscilaguedesdantas@gmail.com>

--- src/fr/eni/projet/bll/UtilisateurManager.java
+++ src/fr/eni/projet/bll/UtilisateurManager.java
@@ -53,51 +53,110 @@ public class UtilisateurManager {
 		daoUtilisateur.insert(u);
 	}
 
-	public static boolean controleInscription(Utilisateur u, String confirmMotDePasse) {
-		
-		// TODO PRISCILA finir méthode	
-		
+	public static String controleInscription(Utilisateur u, String confirmation_mot_de_passe) {
+
+		// TODO PRISCILA check nom, prenom, ville, motDePasse
+
 		List<Utilisateur> utilisateurs = null;
+		String messageErreur = "";
+		boolean motDePasseOk = false;
+		boolean confirmationMotDePasseOk = false;
+		boolean pseudoOk = false;
+		boolean nomOk = false;
+		boolean prenomOk = false;
+		boolean telephoneOk = false;
+		boolean emailOk = false;
+		boolean villeOk = false;
+
+		// initialise la liste d'utilisateurs
+
 		try {
 			utilisateurs = daoUtilisateur.selectAll();
+		} catch (SQLException e1) {
+			// TODO PRISCILA gérer exception
+			e1.printStackTrace();
+		}
+
+
+		//-------------check pseudo
+
+		// verifie s'il y a des caracteres speciaux dans pseudo
+		boolean noSpecialChars = false;
+		char[] charsPseudo = u.getPseudo().toCharArray();
+		for(char c : charsPseudo) {
+			if(c>=48 && c<=57 || c>=65 && c<=90 || c>=97 && c<=122 ) {
+				noSpecialChars=true;
+			} else {
+				noSpecialChars=false;
+				break;
+			}
+		}
+
+		// vérifie si le pseudo existe déjà
+		Utilisateur recherchePseudo = null; 
+		try {
+			recherchePseudo = daoUtilisateur.selectByPseudo(u.getPseudo());
 		} catch (SQLException e) {
 			// TODO PRISCILA gérer exception
 			e.printStackTrace();
 		}
 
-		// check mot de passe 
-		if(!u.getMotDePasse().equals(confirmMotDePasse)) {
-			System.out.println("erreur confimation mot de passe");
-			return false;
+
+		// teste toutes les conditions
+		if(!noSpecialChars) {
+			messageErreur="Le pseudo ne doit pas avoir des caractères spéciaux.";
+		} else if(recherchePseudo != null) {
+			messageErreur="Pseudo déjà utilisé.";
+		} else if (u.getPseudo().contains(" ")) {
+			messageErreur="Le pseudo ne doit pas avoir des espaces.";
+		} else if (u.getPseudo().length()<5 || u.getPseudo().isEmpty()) {
+			messageErreur="Le pseudo doit avoir au moins 5 caracteres";
+		} else if (u.getPseudo().length()>20) {
+			messageErreur="Le pseudo doit avoir au maximum 20 caracteres";
+		} else {
+			pseudoOk=true;
 		}
 
-		//check pseudo
-		for (Utilisateur ut : utilisateurs) {
-			if(ut.getPseudo().equals(u.getPseudo())) {
-				System.out.println("pseudo déjà utilisé");
-				return false;
-			}
+		// -------------------- check confirmation mot de passe
+
+		if(!u.getMotDePasse().equals(confirmation_mot_de_passe)) {
+			messageErreur="Confimation de mot de passe incorrecte.";
+		} else {
+			confirmationMotDePasseOk=true;
 		}
+
+
+		// -------------------- check e-mail
+
+		//TODO PRISCILA créer méthode selectByEmail
 
 		//check e-mail
+
 		for (Utilisateur ut : utilisateurs) {
 			if(ut.getEmail().equals(u.getEmail())) {
 				System.out.println("email déjà utilisé");
-				return false;
 			}
 		}
 
-		//check telephone
+		// ------------------- check telephone
+
+		//TODO PRISCILA créer méthode selectByTelephone
+
 		if(u.getTelephone()!=null) {
 			for (Utilisateur ut : utilisateurs) {
 				if(ut.getTelephone().equals(u.getTelephone())) {
 					System.out.println("telephone déjà utilisé");
-					return false;
 				}
-			}
+			}	
 		}
 
-		return true;
+		// verifier si toutes les conditions sont remplies
+		if(motDePasseOk && confirmationMotDePasseOk  && pseudoOk && nomOk && prenomOk && telephoneOk && emailOk && villeOk) {
+			messageErreur="Verificaton réussite.";
+		}
+
+		return messageErreur;
+
 	}
 
 }
--- src/fr/eni/projet/servlets/ServletVersJSPInscription.java
+++ src/fr/eni/projet/servlets/ServletVersJSPInscription.java
@@ -8,6 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import fr.eni.projet.bll.UtilisateurManager;
 import fr.eni.projet.bo.Utilisateur;
@@ -15,6 +16,7 @@ import fr.eni.projet.bo.Utilisateur;
 /**
  * Servlet implementation class ServletVersJSPInscription
  */
+
 @WebServlet("/ServletVersJSPInscription")
 public class ServletVersJSPInscription extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -33,29 +35,35 @@ public class ServletVersJSPInscription extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		Utilisateur u = new Utilisateur();
+		String confirmation_mot_de_passe = request.getParameter("confirmation_mot_de_passe");
+		HttpSession session = request.getSession();
 		
-		if ((request.getParameter("mot_de_passe")).equals(request.getParameter("confirmation_mot_de_passe"))) {
-			Utilisateur u = new Utilisateur();
-			u.setPseudo(request.getParameter("pseudo"));
-			u.setNom(request.getParameter("nom"));
-			u.setPrenom(request.getParameter("prenom"));
-			u.setEmail(request.getParameter("email"));
-			u.setTelephone(request.getParameter("telephone"));
-			u.setRue(request.getParameter("rue"));
-			u.setCodePostal(request.getParameter("code_postal"));
-			u.setVille(request.getParameter("ville"));
-			u.setMotDePasse(request.getParameter("mot_de_passe"));
-			u.setCredit(0);
-			u.setAdministrateur(false);
+		u.setPseudo(request.getParameter("pseudo"));
+		u.setNom(request.getParameter("nom"));
+		u.setPrenom(request.getParameter("prenom"));
+		u.setEmail(request.getParameter("email"));
+		u.setTelephone(request.getParameter("telephone"));
+		u.setRue(request.getParameter("rue"));
+		u.setCodePostal(request.getParameter("code_postal"));
+		u.setVille(request.getParameter("ville"));
+		u.setMotDePasse(request.getParameter("mot_de_passe"));
+		u.setCredit(0);
+		u.setAdministrateur(false);
+		
+		
+		if (UtilisateurManager.controleInscription(u, confirmation_mot_de_passe).equals("Verificaton réussite.")) {
 			
 			UtilisateurManager.insert(u);
+			session.setAttribute("utilisateur", u);
+			this.getServletContext().getRequestDispatcher("WEB-INF/jspFiles/jspPageDAccueil.jsp").forward(request, response);
 			
-			//TODO PRISCILA diriger vers la page d'accueil en mode logedId
-			RequestDispatcher rd=null;
-			rd = request.getRequestDispatcher("/WEB-INF/jspFiles/jspPageDAccueil.jsp");
-			rd.forward(request, response);
+		} else {
+			
+			session.setAttribute("message_erreur", request.getAttribute("message_erreur"));
 		}
-		//TODO PRISCILA add else pour msg "mots de passe diferents"
+		
 	}
 
 }
