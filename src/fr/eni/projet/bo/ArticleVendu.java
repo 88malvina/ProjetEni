b6d59310b5500abd6e81b6eee35c73781d07c2c0
Priscila Conchou <priscilaguedesdantas@gmail.com>
2021-07-05 19:15:47 +0200

    Ajout de attributs, get/set et constructeurs
    Signed-off-by: Priscila Conchou <priscilaguedesdantas@gmail.com>

--- src/fr/eni/projet/bo/ArticleVendu.java
+++ src/fr/eni/projet/bo/ArticleVendu.java
@@ -2,6 +2,7 @@ package fr.eni.projet.bo;
 
 import java.time.LocalDate;
 public class ArticleVendu { 
+	
 	private int noArticle;
 	private String nomArticle;
 	private String description;
@@ -10,9 +11,89 @@ public class ArticleVendu {
 	private int miseAPrix;
 	private int prixVente;
 	private String etatVente;
+	private int no_utilisateur;
+	private int no_categorie;
 	
+	
+	
+	public ArticleVendu(String nomArticle, String description, LocalDate dateDebutEncheres, LocalDate dateFinEncheres,
+			int no_utilisateur, int no_categorie) {
+		super();
+		this.nomArticle = nomArticle;
+		this.description = description;
+		this.dateDebutEncheres = dateDebutEncheres;
+		this.dateFinEncheres = dateFinEncheres;
+		this.no_utilisateur = no_utilisateur;
+		this.no_categorie = no_categorie;
+	}
+
+	public ArticleVendu(String nomArticle, String description, LocalDate dateDebutEncheres, LocalDate dateFinEncheres,
+			int prixVente, int no_utilisateur, int no_categorie) {
+		super();
+		this.nomArticle = nomArticle;
+		this.description = description;
+		this.dateDebutEncheres = dateDebutEncheres;
+		this.dateFinEncheres = dateFinEncheres;
+		this.prixVente = prixVente;
+		this.no_utilisateur = no_utilisateur;
+		this.no_categorie = no_categorie;
+	}
+
+	public ArticleVendu(String nomArticle, String description, LocalDate dateDebutEncheres, LocalDate dateFinEncheres,
+			int miseAPrix, int prixVente, int no_utilisateur, int no_categorie) {
+		super();
+		this.nomArticle = nomArticle;
+		this.description = description;
+		this.dateDebutEncheres = dateDebutEncheres;
+		this.dateFinEncheres = dateFinEncheres;
+		this.miseAPrix = miseAPrix;
+		this.prixVente = prixVente;
+		this.no_utilisateur = no_utilisateur;
+		this.no_categorie = no_categorie;
+	}
+
+	public ArticleVendu(String nomArticle, String description, LocalDate dateDebutEncheres, LocalDate dateFinEncheres,
+			String etatVente, int no_utilisateur, int no_categorie) {
+		super();
+		this.nomArticle = nomArticle;
+		this.description = description;
+		this.dateDebutEncheres = dateDebutEncheres;
+		this.dateFinEncheres = dateFinEncheres;
+		this.etatVente = etatVente;
+		this.no_utilisateur = no_utilisateur;
+		this.no_categorie = no_categorie;
+	}
+
+	public ArticleVendu(String nomArticle, String description, LocalDate dateDebutEncheres, LocalDate dateFinEncheres,
+			int prixVente, String etatVente, int no_utilisateur, int no_categorie) {
+		super();
+		this.nomArticle = nomArticle;
+		this.description = description;
+		this.dateDebutEncheres = dateDebutEncheres;
+		this.dateFinEncheres = dateFinEncheres;
+		this.prixVente = prixVente;
+		this.etatVente = etatVente;
+		this.no_utilisateur = no_utilisateur;
+		this.no_categorie = no_categorie;
+	}
+
+	public ArticleVendu(String nomArticle, String description, LocalDate dateDebutEncheres, LocalDate dateFinEncheres,
+			int miseAPrix, int prixVente, String etatVente, int no_utilisateur, int no_categorie) {
+		super();
+		this.nomArticle = nomArticle;
+		this.description = description;
+		this.dateDebutEncheres = dateDebutEncheres;
+		this.dateFinEncheres = dateFinEncheres;
+		this.miseAPrix = miseAPrix;
+		this.prixVente = prixVente;
+		this.etatVente = etatVente;
+		this.no_utilisateur = no_utilisateur;
+		this.no_categorie = no_categorie;
+	}
+
 	public ArticleVendu(int noArticle, String nomArticle, String description, LocalDate dateDebutEncheres,
-			LocalDate dateFinEncheres, int miseAPrix, int prixVente, String etatVente) {
+			LocalDate dateFinEncheres, int miseAPrix, int prixVente, String etatVente, int no_utilisateur,
+			int no_categorie) {
 		super();
 		this.noArticle = noArticle;
 		this.nomArticle = nomArticle;
@@ -22,19 +103,36 @@ public class ArticleVendu {
 		this.miseAPrix = miseAPrix;
 		this.prixVente = prixVente;
 		this.etatVente = etatVente;
+		this.no_utilisateur = no_utilisateur;
+		this.no_categorie = no_categorie;
 	}
+	
+	//---------------- constructeurs faits par Malvina (sans no_utilisateur et no_categorie)
 
-	public ArticleVendu(String nomArticle, String description, LocalDate dateDebutEncheres, LocalDate dateFinEncheres,
-			int miseAPrix, int prixVente, String etatVente) {
-		super();
-		this.nomArticle = nomArticle;
-		this.description = description;
-		this.dateDebutEncheres = dateDebutEncheres;
-		this.dateFinEncheres = dateFinEncheres;
-		this.miseAPrix = miseAPrix;
-		this.prixVente = prixVente;
-		this.etatVente = etatVente;
-	}
+//	public ArticleVendu(int noArticle, String nomArticle, String description, LocalDate dateDebutEncheres,
+//			LocalDate dateFinEncheres, int miseAPrix, int prixVente, String etatVente) {
+//		super();
+//		this.noArticle = noArticle;
+//		this.nomArticle = nomArticle;
+//		this.description = description;
+//		this.dateDebutEncheres = dateDebutEncheres;
+//		this.dateFinEncheres = dateFinEncheres;
+//		this.miseAPrix = miseAPrix;
+//		this.prixVente = prixVente;
+//		this.etatVente = etatVente;
+//	}
+//
+//	public ArticleVendu(String nomArticle, String description, LocalDate dateDebutEncheres, LocalDate dateFinEncheres,
+//			int miseAPrix, int prixVente, String etatVente) {
+//		super();
+//		this.nomArticle = nomArticle;
+//		this.description = description;
+//		this.dateDebutEncheres = dateDebutEncheres;
+//		this.dateFinEncheres = dateFinEncheres;
+//		this.miseAPrix = miseAPrix;
+//		this.prixVente = prixVente;
+//		this.etatVente = etatVente;
+//	}
 
 	public ArticleVendu() {
 	}
@@ -101,6 +199,22 @@ public class ArticleVendu {
 
 	public void setEtatVente(String etatVente) {
 		this.etatVente = etatVente;
+	}
+
+	public int getNo_utilisateur() {
+		return no_utilisateur;
+	}
+
+	public void setNo_utilisateur(int no_utilisateur) {
+		this.no_utilisateur = no_utilisateur;
+	}
+
+	public int getNo_categorie() {
+		return no_categorie;
+	}
+
+	public void setNo_categorie(int no_categorie) {
+		this.no_categorie = no_categorie;
 	}
 
 	@Override
