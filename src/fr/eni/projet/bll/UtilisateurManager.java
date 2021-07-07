9e22017865f6b960f3d85ff455a091ead00277ec
Antoine Blanchet <antoine.blanchet2021@campus-eni.fr>
2021-07-07 17:19:49 +0200

    controle modif mot de passe recopiée sur celle de création compte

--- src/fr/eni/projet/bll/UtilisateurManager.java
+++ src/fr/eni/projet/bll/UtilisateurManager.java
@@ -261,7 +261,6 @@ public class UtilisateurManager {
 
 		if(u.getMotDePasse().equals(u.getPrenom()+123) || u.getMotDePasse().equals(u.getNom()+123)){
 			messageErreur="La securité du mot de passe est trop faible.";
-			// TODO ANTOINE : corriger la méthode copié
 		} else if(u.getMotDePasse().equals(u.getPseudo()+123)) {
 			messageErreur="La securité du mot de passe est trop faible.";
 		} else if(u.getMotDePasse().equals("motdepasse")) {
@@ -362,13 +361,12 @@ public class UtilisateurManager {
 	}
 
 	//  ==========================================================================
-
-	// C'est la même méthode que pour l'inscription à la différence que l'on prend aussi en paramètre
+	
+	// 	C'est la même méthode que pour l'inscription à la différence que l'on prend aussi en paramètre
 	// l'utilisateur en attribut de session avant les mises à jours
 	// Pour pouvoir gérer le fait que lors d'une mise à jour de profil, on a le droit de garder son pseudo ou email déjà existant
 	// (même si celui-ci sera déjà en BDD)
-
-
+	
 	/**
 	 * 
 	 * @param u l'utilisateur avec les infos que l'on veut mettre à jour (après la saisie de formulaire)
@@ -376,8 +374,10 @@ public class UtilisateurManager {
 	 * @param confirmation_mot_de_passe
 	 * @return
 	 */
-	public String ControleModifProfil(Utilisateur u, Utilisateur old, String confirmation_mot_de_passe) {
-
+	public String ControleModifProfil (Utilisateur u, Utilisateur old, String confirmation_mot_de_passe) {
+		
+		//// START COPIE
+		
 		String messageErreur = "";
 		boolean motDePasseOk = false;
 		boolean confirmationMotDePasseOk = false;
@@ -386,85 +386,90 @@ public class UtilisateurManager {
 		boolean prenomOk = false;
 		boolean telephoneOk = false;
 		boolean emailOk = false;
+		boolean codePostalOk = false;
 
 		//-------------check pseudo
 
-		// verifie s'il y a des caracteres speciaux dans pseudo
-		boolean noSpecialChars = false;
-		char[] charsPseudo = u.getPseudo().toCharArray();
-		for(char c : charsPseudo) {
-			if(c>=48 && c<=57 || c>=65 && c<=90 || c>=97 && c<=122 ) {
-				noSpecialChars=true;
-			} else {
-				noSpecialChars=false;
-				break;
-			}
-		}
-
-		// vérifie si le pseudo existe déjà ou si c'est le pseudo de l'utilisateur old
+		// vérifie si le pseudo existe déjà
 		Utilisateur recherche = null; 
-
 		try {
 			recherche = daoUtilisateur.selectByPseudo(u.getPseudo());
-			System.out.println(old.getPseudo());
-			System.out.println(u.getPseudo());
 			if (old.getPseudo().equals(u.getPseudo())) {
 				recherche = null;
 			}
-
 		} catch (SQLException e) {
-			// TODO ANTOINE gérer exception
+			// TODO PRISCILA gérer exception
 			e.printStackTrace();
 		}
 
 		// teste toutes les conditions
-		if(!noSpecialChars) {
+		if(!OutilsVerification.noSpecialChars(u.getPseudo())) {
 			messageErreur="Le pseudo ne doit pas avoir des caractères spéciaux.";
 		} else if(recherche != null) {
 			messageErreur="Pseudo déjà utilisé.";
 		} else if (u.getPseudo().contains(" ")) {
 			messageErreur="Le pseudo ne doit pas avoir des espaces.";
-		} else if (u.getPseudo().length()<5 || u.getPseudo().isEmpty()) {
-			messageErreur="Le pseudo doit avoir au moins 5 caracteres.";
+		} else if (u.getPseudo().length()<4 || u.getPseudo().isEmpty()) {
+			messageErreur="Le pseudo doit avoir au moins 4 caracteres.";
 		} else if (u.getPseudo().length()>20) {
 			messageErreur="Le pseudo doit avoir au maximum 20 caracteres.";
 		} else {
 			pseudoOk=true;
 		}
-
-		System.out.println(messageErreur);
+		
+		System.out.println("verif pseudo est passé ");
+		System.out.println(pseudoOk);
 
 		// -------------------- check nom
 
-		if(u.getNom().length()<2) {
+		if(u.getNom().length()<3) {
 			messageErreur="Le nom doit avoir au moins 3 caracteres.";
 		} else if (u.getNom().length()>20) {
 			messageErreur="Le nom doit avoir au maximum 20 caracteres.";
+		} else if (!OutilsVerification.onlyLetters(u.getNom())) {
+			messageErreur="Le nom ne doit pas avoir des caractères spéciaux.";
 		} else {
 			nomOk=true;
 		}
+		
+		System.out.println("verif nom est passé");
+		System.out.println(nomOk);
 
 		// -------------------- check prenom
 
-		if(u.getPrenom().length()<2) {
+		if(u.getPrenom().length()<3) {
 			messageErreur="Le prenom doit avoir au moins 3 caracteres.";
 		} else if (u.getPrenom().length()>20) {
 			messageErreur="Le prenom doit avoir au maximum 20 caracteres.";
+		} else if (!OutilsVerification.onlyLetters(u.getPrenom())) {
+			messageErreur="Le prenom ne doit pas avoir des caractères spéciaux.";
 		} else {
 			prenomOk=true;
 		}
+		
+		System.out.println("verif prenom est passé");
+		System.out.println(prenomOk);
 
 		// -------------------- check mot de passe
 
 		if(u.getMotDePasse().equals(u.getPrenom()+123) || u.getMotDePasse().equals(u.getNom()+123)){
 			messageErreur="La securité du mot de passe est trop faible.";
-		} else if(u.getMotDePasse().equals("motdepasse")) {
+		} else if(u.getMotDePasse().equals(u.getPseudo()+123)) {
 			messageErreur="La securité du mot de passe est trop faible.";
 		} else if(u.getMotDePasse().equals("motdepasse")) {
 			messageErreur="La securité du mot de passe est trop faible.";
+		} else if (u.getMotDePasse().length()<8 || u.getPseudo().isEmpty()) {
+			messageErreur="Le mot de passe doit avoir au moins 8 caracteres.";
+		} else if (u.getMotDePasse().length()>20) {
+			messageErreur="Le mot de passe doit avoir au maximum 20 caracteres.";
+		} else if (OutilsVerification.noSpecialChars(u.getMotDePasse())) {
+			messageErreur="Le mot de passe doit avoir au moins 1 caractere special.";
 		} else {
 			motDePasseOk=true;
 		}
+		
+		System.out.println("verif mdp est passé");
+		System.out.println(motDePasseOk);
 
 		// -------------------- check confirmation mot de passe
 
@@ -474,9 +479,12 @@ public class UtilisateurManager {
 			confirmationMotDePasseOk=true;
 		}
 
-		System.out.println(messageErreur);
+		System.out.println("verif mdp confirm est passé");
+		System.out.println(confirmationMotDePasseOk);
 
 		// -------------------- check e-mail
+
+		// check si l'e-mail est déjà utilisé
 
 		recherche = null; 
 		try {
@@ -484,57 +492,245 @@ public class UtilisateurManager {
 			if (old.getEmail().equals(u.getEmail())) {
 				recherche = null;
 			}
-
 		} catch (SQLException e) {
-			// TODO ANTOINE gérer exception
+			// TODO PRISCILA gérer exception
 			e.printStackTrace();
 		}
 		if(recherche==null) {
 			emailOk=true;
+		} else {
+			messageErreur="Email déjà utilisé.";
 		}
 
 		System.out.println(messageErreur);
+		System.out.println("verif email est passé");
+		System.out.println(emailOk);
 
 		// ------------------- check telephone
 
 		recherche = null; 
 		try {
 			recherche = daoUtilisateur.selectByTelephone(u.getTelephone());
+			
 			if(old.getTelephone().equals(u.getTelephone())) {
 				recherche = null;
 			}
 		} catch (SQLException e) {
-			// TODO ANTOINE gérer exception
+			// TODO PRISCILA gérer exception
+			
 			e.printStackTrace();
 		}
+		
 		if(recherche==null) {
 			telephoneOk=true;
+		} else if(u.getTelephone().equals("")) {
+			telephoneOk=true;
+		} else {
+			messageErreur="Ce téléphone est déjà utilisé";
 		}
 
 		System.out.println(messageErreur);
+		
+		System.out.println("verif tel est passé");
+		System.out.println(telephoneOk);
+		
+		// ------------------- check code postal
+		
+		if(!OutilsVerification.onlyNumbers(u.getCodePostal()) || u.getCodePostal().length()<5) {
+			messageErreur="Le code postal doit avoir 5 chiffres.";
+		} else {
+			codePostalOk=true;
+		}
+		
+		System.out.println("verif c est passé");
+		System.out.println(codePostalOk);
 
 		// ---------------------- verifier si toutes les conditions sont remplies
-		// Petite modification versus l'inscription : l'utilisateur a le droit de reprendre le même pseudo ou email. Celui ci est
-		// déjà dans la base, mais c'est bon quand même si c'est le sien
 
-		if(
-				motDePasseOk 
-				&& confirmationMotDePasseOk
-				&& pseudoOk
-				&& nomOk 
-				&& prenomOk 
-				&& telephoneOk 
-				&& emailOk
-				) 
-		{
+		if(motDePasseOk && confirmationMotDePasseOk  && pseudoOk && nomOk && prenomOk && telephoneOk && emailOk && codePostalOk) {
 			messageErreur="Verificaton réussite.";
 		}
 
 		System.out.println(messageErreur);
 
 		return messageErreur;
-
 	}
+	
+	
+//	//  BACKUP        ==========================================================================
+//
+//	// C'est la même méthode que pour l'inscription à la différence que l'on prend aussi en paramètre
+//	// l'utilisateur en attribut de session avant les mises à jours
+//	// Pour pouvoir gérer le fait que lors d'une mise à jour de profil, on a le droit de garder son pseudo ou email déjà existant
+//	// (même si celui-ci sera déjà en BDD)
+//
+//
+//	/**
+//	 * 
+//	 * @param u l'utilisateur avec les infos que l'on veut mettre à jour (après la saisie de formulaire)
+//	 * @param old l'utilisateur initial (avant saisie de formulaire)
+//	 * @param confirmation_mot_de_passe
+//	 * @return
+//	 */
+//	public String ControleModifProfil(Utilisateur u, Utilisateur old, String confirmation_mot_de_passe) {
+//
+//		String messageErreur = "";
+//		boolean motDePasseOk = false;
+//		boolean confirmationMotDePasseOk = false;
+//		boolean pseudoOk = false;
+//		boolean nomOk = false;
+//		boolean prenomOk = false;
+//		boolean telephoneOk = false;
+//		boolean emailOk = false;
+//
+//		//-------------check pseudo
+//
+//		// verifie s'il y a des caracteres speciaux dans pseudo
+//		boolean noSpecialChars = false;
+//		char[] charsPseudo = u.getPseudo().toCharArray();
+//		for(char c : charsPseudo) {
+//			if(c>=48 && c<=57 || c>=65 && c<=90 || c>=97 && c<=122 ) {
+//				noSpecialChars=true;
+//			} else {
+//				noSpecialChars=false;
+//				break;
+//			}
+//		}
+//
+//		// vérifie si le pseudo existe déjà ou si c'est le pseudo de l'utilisateur old
+//		Utilisateur recherche = null; 
+//
+//		try {
+//			recherche = daoUtilisateur.selectByPseudo(u.getPseudo());
+//			System.out.println(old.getPseudo());
+//			System.out.println(u.getPseudo());
+//			if (old.getPseudo().equals(u.getPseudo())) {
+//				recherche = null;
+//			}
+//
+//		} catch (SQLException e) {
+//			// TODO ANTOINE gérer exception
+//			e.printStackTrace();
+//		}
+//
+//		// teste toutes les conditions
+//		if(!noSpecialChars) {
+//			messageErreur="Le pseudo ne doit pas avoir des caractères spéciaux.";
+//		} else if(recherche != null) {
+//			messageErreur="Pseudo déjà utilisé.";
+//		} else if (u.getPseudo().contains(" ")) {
+//			messageErreur="Le pseudo ne doit pas avoir des espaces.";
+//		} else if (u.getPseudo().length()<5 || u.getPseudo().isEmpty()) {
+//			messageErreur="Le pseudo doit avoir au moins 5 caracteres.";
+//		} else if (u.getPseudo().length()>20) {
+//			messageErreur="Le pseudo doit avoir au maximum 20 caracteres.";
+//		} else {
+//			pseudoOk=true;
+//		}
+//
+//		System.out.println(messageErreur);
+//
+//		// -------------------- check nom
+//
+//		if(u.getNom().length()<2) {
+//			messageErreur="Le nom doit avoir au moins 3 caracteres.";
+//		} else if (u.getNom().length()>20) {
+//			messageErreur="Le nom doit avoir au maximum 20 caracteres.";
+//		} else {
+//			nomOk=true;
+//		}
+//
+//		// -------------------- check prenom
+//
+//		if(u.getPrenom().length()<2) {
+//			messageErreur="Le prenom doit avoir au moins 3 caracteres.";
+//		} else if (u.getPrenom().length()>20) {
+//			messageErreur="Le prenom doit avoir au maximum 20 caracteres.";
+//		} else {
+//			prenomOk=true;
+//		}
+//
+//		// -------------------- check mot de passe
+//
+//		if(u.getMotDePasse().equals(u.getPrenom()+123) || u.getMotDePasse().equals(u.getNom()+123)){
+//			messageErreur="La securité du mot de passe est trop faible.";
+//		} else if(u.getMotDePasse().equals("motdepasse")) {
+//			messageErreur="La securité du mot de passe est trop faible.";
+//		} else if(u.getMotDePasse().equals("motdepasse")) {
+//			messageErreur="La securité du mot de passe est trop faible.";
+//		} else {
+//			motDePasseOk=true;
+//		}
+//
+//		// -------------------- check confirmation mot de passe
+//
+//		if(!u.getMotDePasse().equals(confirmation_mot_de_passe)) {
+//			messageErreur="Confimation de mot de passe incorrecte.";
+//		} else {
+//			confirmationMotDePasseOk=true;
+//		}
+//
+//		System.out.println(messageErreur);
+//
+//		// -------------------- check e-mail
+//
+//		recherche = null; 
+//		try {
+//			recherche = daoUtilisateur.selectByEmail(u.getEmail());
+//			if (old.getEmail().equals(u.getEmail())) {
+//				recherche = null;
+//			}
+//
+//		} catch (SQLException e) {
+//			// TODO ANTOINE gérer exception
+//			e.printStackTrace();
+//		}
+//		if(recherche==null) {
+//			emailOk=true;
+//		}
+//
+//		System.out.println(messageErreur);
+//
+//		// ------------------- check telephone
+//
+//		recherche = null; 
+//		try {
+//			recherche = daoUtilisateur.selectByTelephone(u.getTelephone());
+//			if(old.getTelephone().equals(u.getTelephone())) {
+//				recherche = null;
+//			}
+//		} catch (SQLException e) {
+//			// TODO ANTOINE gérer exception
+//			e.printStackTrace();
+//		}
+//		if(recherche==null) {
+//			telephoneOk=true;
+//		}
+//
+//		System.out.println(messageErreur);
+//
+//		// ---------------------- verifier si toutes les conditions sont remplies
+//		// Petite modification versus l'inscription : l'utilisateur a le droit de reprendre le même pseudo ou email. Celui ci est
+//		// déjà dans la base, mais c'est bon quand même si c'est le sien
+//
+//		if(
+//				motDePasseOk 
+//				&& confirmationMotDePasseOk
+//				&& pseudoOk
+//				&& nomOk 
+//				&& prenomOk 
+//				&& telephoneOk 
+//				&& emailOk
+//				) 
+//		{
+//			messageErreur="Verificaton réussite.";
+//		}
+//
+//		System.out.println(messageErreur);
+//
+//		return messageErreur;
+//
+//	}
 
 
 
