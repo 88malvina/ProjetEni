b753116ca84398bf14f1f1a1a1e1ffc86394f0d5
Malvina <malvina.avetyan2021@campus-eni.fr>
2021-07-09 04:16:24 +0200

    ajout des méthodes

--- src/fr/eni/projet/dal/DAOArticleVendu.java
+++ src/fr/eni/projet/dal/DAOArticleVendu.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.sql.SQLException;
 
 import fr.eni.projet.bo.ArticleVendu;
+import fr.eni.projet.businessException.BusinessException;
 
 /**
  * @author pconchou2021
@@ -23,5 +24,21 @@ public interface DAOArticleVendu extends DAO<ArticleVendu> {
 	public List<ArticleVendu> selectAvecPseudo() throws SQLException;
 	
 	public List<ArticleVendu> selectVentesNonDebutees();
+	
+	public ArticleVendu selectNomByNumero(int id) throws BusinessException;
+	
+	public List<ArticleVendu> selectVentesNonDebuteesParCategorie(int no_categorie) throws BusinessException;
+	
+	public List<ArticleVendu> selectMesEncheresEnCours(int no_categorie) throws BusinessException;
+	
+	public List<ArticleVendu> selectMesEncheresTermineesByCategorie(int no_categorie) throws BusinessException;
+	
+	public List<ArticleVendu> selectMesEncheresEnCoursByCategorieEtNom(int no_categorie,String nom) throws BusinessException;
+	
+	public List<ArticleVendu> selectMesEncheresNonDebuteesByCategorieEtNom(int no_categorie,String nom) throws BusinessException;
+	
+	public List<ArticleVendu> selectMesEncheresTermineesByCategorieEtNom(int no_categorie,String nom) throws BusinessException;
+	
+	
 	
 }
